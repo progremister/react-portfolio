f02f8b81ea482af7d60ad21cc55b473d6b397f36
Dmytro Kolosovskyi <Dmytro_Kolosovskyi@mac2018.fvt.sk>
2023-02-07 11:35:04 +0100

    Connected the Sanity CRM to the frontend part

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -18,11 +18,6 @@ import sass from '../assets/sass.png';
 import typescript from '../assets/typescript.png';
 import vue from '../assets/vue.png';
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
-
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
@@ -55,10 +50,6 @@ export default {
     sass,
     typescript,
     vue,
-    about01,
-    about02,
-    about03,
-    about04,
     profile,
     circle,
     logo,
--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,31 +3,17 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
-
-const abouts = [
-    {
-        title: 'Frontend Development',
-        description: 'I am able to make your website modern and interactive.',
-        imgUrl: images.about04
-    },
-    {
-        title: 'Backend Development',
-        description: 'I will make yours website functionality wide and effective.',
-        imgUrl: images.about01
-    },
-    {
-        title: 'UX/UI design',
-        description: 'You will get an attractive and eye-catching design.',
-        imgUrl: images.about02
-    },
-    {
-        title: 'SEO',
-        description: 'I am a good SEO manager, so that your product will be on the top of the search queries.',
-        imgUrl: images.about03
-    },
-];
+import { urlFor, client } from "../../client";
 
 const About = () => {
+
+    const [abouts, setAbouts] = useState([]);
+    useEffect(() => {
+        const query = '*[_type == "abouts"]';
+        client.fetch(query)
+            .then((data) => setAbouts(data));
+    }, []);
+
     return (
         <>
             <h2 className="head-text">
@@ -42,7 +28,7 @@ const About = () => {
                             className="app__profile-item"
                             key={about.title + index}
                         >
-                            <img src={about.imgUrl} alt={about.title}/>
+                            <img src={urlFor(about.imgUrl)} alt={about.title}/>
                             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
                             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
                         </motion.div>
