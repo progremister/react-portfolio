d3a0f69b33bcde2096896ee0e8cafe336d4a9dc8
Dmytro Kolosovskyi <Dmytro_Kolosovskyi@mac2018.fvt.sk>
2023-02-08 16:35:45 +0100

    Added the MotionWrapper with animation for each section

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -87,4 +87,8 @@ const Skills = () => {
         </>
     );
 };
-export default AppWrap( Skills, 'skills' );
+export default AppWrap(
+    MotionWrap(Skills, 'app__skills'),
+    'skills',
+    "app__whitebg"
+);
