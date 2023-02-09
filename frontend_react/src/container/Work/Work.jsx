a0c19a28c7dc072b0606743e1fe71f4a7c30dec1
Dmytro Kolosovskyi <Dmytro_Kolosovskyi@mac2018.fvt.sk>
2023-02-09 14:50:05 +0100

    Added extra links to works

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -87,7 +87,9 @@ const Work = () => {
                             </motion.div>
                         </div>
                         <div className="app__work-content app__flex">
-                            <h4 className="bold-text">{work.title}</h4>
+                            <a href={ work.projectLink} target="_blank" rel="noreferrer">
+                                <h4 className="bold-text">{work.title}</h4>
+                            </a>
                             <p className="p-text" style={{ marginTop: 10}}>{work.description}</p>
                             <div className="app__work-tag app__flex">
                                 <p className="p-text">{work.tags[0]}</p>
